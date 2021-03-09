468c4ee46ee3abb8aebc9c7493b3dbd22a834382
Bixcoitoo <isacmagalha123@gmail.com>
2021-03-09 01:05:35 -0300

    First commit

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 TIU 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5573988453021*
+║
+╚═〘  TIUBIXCOITO BOT 〙
+`
+}
+exports.iklan = iklan
